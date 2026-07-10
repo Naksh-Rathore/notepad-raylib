51ddd3e253a4611919af37d0c4e910ebfc210bd5
Naksh Rathore <naksh.rathore786@gmail.com>
2026-07-10 18:13:00 -0500

    Stop tracking ignored files

--- build/CMakeFiles/notepad-raylib.dir/src/main.c.o.d
+++
@@ -1,6 +0,0 @@
-CMakeFiles/notepad-raylib.dir/src/main.c.o: \
- /home/nakshrathore/programming/notepad-raylib/src/main.c \
- /usr/include/stdc-predef.h \
- /home/nakshrathore/programming/notepad-raylib/build/_deps/raylib-src/src/raylib.h \
- /usr/lib/gcc/x86_64-redhat-linux/16/include/stdarg.h \
- /usr/lib/gcc/x86_64-redhat-linux/16/include/stdbool.h
