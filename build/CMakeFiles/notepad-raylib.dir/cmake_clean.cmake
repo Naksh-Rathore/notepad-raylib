66a84025105cec5f823141e8ef41a4ae5d8b66a9
Naksh Rathore <naksh.rathore786@gmail.com>
2026-07-10 18:10:57 -0500

    first commit

---
+++ build/CMakeFiles/notepad-raylib.dir/cmake_clean.cmake
@@ -0,0 +1,12 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/notepad-raylib.dir/link.d"
+  "CMakeFiles/notepad-raylib.dir/src/main.c.o"
+  "CMakeFiles/notepad-raylib.dir/src/main.c.o.d"
+  "notepad-raylib"
+  "notepad-raylib.pdb"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang C)
+  include(CMakeFiles/notepad-raylib.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
